fbcf1b7c6b63b8e9d687b7c0eeaa3abfb4fd680e
Payas Parab <payas.parab@ninepointtwo.com>
2021-08-02 19:01:43 -0400

    latest push

--- crypto_db.py
+++ crypto_db.py
@@ -39,6 +39,7 @@ class CryptoDB:
         self.raw_data_path = raw_data_path
         self.store = pystore.store('CryptoDB')
         self.pairs = self.kraken_api.get_tradable_asset_pairs()
+        self.assetcodes = pd.read_csv('assetcode_key.csv')
 
     def process_csv(self, file_name): 
         '''
@@ -55,19 +56,23 @@ class CryptoDB:
         '''
         # Pull in file #
         full_path = self.raw_data_path + '\\' + file_name
+        pair_code = file_name[:-4]
         cols = ['timestamp', 'price', 'volume']
         data = pd.read_csv(full_path, names=cols, header=None)
+        data['pair_code'] = pair_code
         
         # Process Dates #
         data['timestamp'] = pd.to_datetime(data['timestamp'].apply(
                 lambda x: datetime.fromtimestamp(x)))
 
+        # Asset Code Processing #
         
+
         return data
         
 
-
-# Function Read CSV and Convert to Data Frame #
+    def dump_to_pystore(self): 
+        pass
 
 
 # Function to Calculate Volume Weighted Price takes in time period #
