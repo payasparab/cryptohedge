4f6642c1bacc977dae7bf59d48135421e8959409
Payas Parab <payas.parab@ninepointtwo.com>
2021-08-02 13:33:01 -0400

    crypto_db: first commit to get the database started

---
+++ crypto_db.py
@@ -0,0 +1,21 @@
+import pandas as pd 
+import pystore
+import numpy as np
+
+pystore_path =  os.path.dirname(os.path.abspath(__file__)) + '\\data'
+class CryptoDB:
+    def __init__(self, pystore_path, raw_data_path): 
+    
+        pystore.set_path(path)
+        self.pystore_path = pystore.get_path()
+        self.raw_data_path = 
+        store_name = 'CryptoDB'
+        self.store = pystore.store(store_name)
+
+    def process_csv(file_name)
+        
+
+# Function Read CSV and Convert to Data Frame #
+pd.to_datetime(datetime.fromtimestamp())
+
+# Function to Calculate Volume Weighted Price takes in time period #
