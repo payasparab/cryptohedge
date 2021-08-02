ea71fbb7288df37bc8e77b7e8c07275a3ae31857
Payas Parab <payas.parab@ninepointtwo.com>
2021-08-02 14:30:30 -0400

    crypto_db: shelled out database code and also included a git ignore for excluding raw data

--- crypto_db.py
+++ crypto_db.py
@@ -1,18 +1,50 @@
 import pandas as pd 
 import pystore
+import os
 import numpy as np
 
-pystore_path =  os.path.dirname(os.path.abspath(__file__)) + '\\data'
-class CryptoDB:
-    def __init__(self, pystore_path, raw_data_path): 
-    
-        pystore.set_path(path)
-        self.pystore_path = pystore.get_path()
-        self.raw_data_path = 
-        store_name = 'CryptoDB'
-        self.store = pystore.store(store_name)
+pystore_path_local =  os.path.dirname(os.path.abspath(__file__)) + '\\pystore'
+raw_data_path_local = os.path.dirname(os.path.abspath(__file__)) + '\\raw_data'
 
-    def process_csv(file_name)
+class CryptoDB:
+    '''
+    class CryptoDB
+
+    Description: a pystore accessor database to quickly access time 
+    series data for cryptocurrency portfolio optimization. 
+    
+    Key Methods: 
+
+        - dbmap : method to access information about items held in CryptoDB
+        - query : access a Pystore item directly
+        
+
+    Key Attributes:
+        - metadata : dict : information about the data currently stored
+    
+    '''
+    
+    def __init__(self, 
+            pystore_path=pystore_path_local, 
+            raw_data_path=raw_data_path_local
+        ): 
+    
+        pystore.set_path(pystore_path)
+        self.pystore_path = pystore.get_path()
+        self.raw_data_path = raw_data_path
+        self.store = pystore.store('CryptoDB')
+
+    def process_csv(file_name): 
+        '''
+        Name: process_csv
+
+        Arguments: 
+            file_name: str : raw csv file name stored in
+
+        Returns: 
+            - pd.DataFrame: 
+                > contains 
+        '''
         
 
 # Function Read CSV and Convert to Data Frame #
