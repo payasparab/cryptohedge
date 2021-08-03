18b48f10b789639462e5f323df260b0e027f11c6
Payas Parab <payas.parab@ninepointtwo.com>
2021-08-03 15:59:38 -0400

    crpyto_factors; made traded volume and wv price dynamic to a user specified time period

--- crypto_factors.py
+++ crypto_factors.py
@@ -2,10 +2,16 @@ import pandas as pd
 import numpy as np
 from crypto_db import CryptoDB
 
-def _calculate_volume_weighted_price(cdb=CryptoDB(), coin='BTC', 
-        time='24H'): 
+def _calculate_volume_weighted_price(coin, cdb=CryptoDB(),time='24H'): 
     '''
+    Args: 
+        - coin : str : currency pair/name
+        - cdb : CrypCtoDB obj : database to query for price data
+        - time : pd time str : rebalance time period for calculating price
     
+    Returns: pd.DF: cols: 
+        - wv_price : float : volume weighted price
+        - traded_volume : float : total traded volume
     '''
     transactions = cdb.query('transactions', coin)
     wv_price = transactions.groupby(pd.Grouper(
@@ -20,8 +26,10 @@ def _calculate_volume_weighted_price(cdb=CryptoDB(), coin='BTC',
             lambda x: (x.volume.sum())
         )
 
-    
-    
+    wv_price = wv_price.rename('wv_price')
+    traded_volume = traded_volume.rename('traded_volume')
+    output = pd.concat([wv_price, traded_volume], axis=1)
+    return  output
 
     
     
