356af0c988d49b079911ccd39f127e8db3f8af3f
Payas Parab <payas.parab@ninepointtwo.com>
2021-08-04 02:05:48 -0400

    market_model: completed a slippage calculator that takes in currency, order type, amount

--- Market_model.py
+++ Market_model.py
@@ -4,7 +4,7 @@ import numpy as np
 from datetime import datetime
 
 # get bid and ask spread
-def slippage_moment(ticker, order_type, amount):
+def crypto_slippage(ticker, order_type, amount):
     '''
     ticker : str : cryptocurrency you want current market liq for
     order_type : str : buy / sell 
@@ -56,12 +56,11 @@ def slippage_moment(ticker, order_type, amount):
             )
             print('Please make a smaller trade you dipshit!')
         elif volume_to_move < ask_df.iloc[0].volume:
-            return (ask_df.iloc[0].price/curr_mkt_price)- 1 
+            return (ask_df.iloc[0].price/curr_mkt_price) - 1 
         else:
-            _orderbook_cleared = ask_df[(ask_df.cumul_volume < volume_to_move)]
-            _full_clear =  _orderbook_cleared.iloc[:-1]
+            _full_clear = ask_df[(ask_df.cumul_volume < volume_to_move)]
             
-            _final_price = _orderbook_cleared.iloc[-1]
+            _final_price = ask_df.iloc[(_full_clear.index.max() + 1)]
             _final_price['volume'] = (volume_to_move - _full_clear.volume.sum())
 
             _total_clear = pd.concat([_full_clear, _final_price.to_frame().T])
@@ -84,41 +83,16 @@ def slippage_moment(ticker, order_type, amount):
         elif volume_to_move < bid_df.iloc[0].volume:
             return (bid_df.iloc[0].price/curr_mkt_price)- 1 
         else:
-            _orderbook_cleared = bid_df[(bid_df.cumul_volume < volume_to_move)]
-            _full_clear =  _orderbook_cleared.iloc[:-1]
+            _full_clear = bid_df[(bid_df.cumul_volume < volume_to_move)]
             
-            _final_price = _orderbook_cleared.iloc[-1]
+            _final_price = bid_df.iloc[(_full_clear.index.max() + 1)]
             _final_price['volume'] = (volume_to_move - _full_clear.volume.sum())
 
             _total_clear = pd.concat([_full_clear, _final_price.to_frame().T])
             exec_price = (_total_clear.price.dot(_total_clear.volume))/ _total_clear.volume.sum()
-            return ((exec_price/curr_mkt_price) - 1)
+            return ((exec_price/curr_mkt_price) - 1).abs()
 
 
-
-
-#Calculate slippage
-
-def market_slippage(crypto, amount, starttime, endtime):
-    bids = []
-
-#get max and min of bids then take average
-    for bid in bids:
-        max_bid = bids.max()
-        min_bid = bids.min()
-        current_market_price = avg(max_bid, min_bid)
-
-# need different groupings for bid and asks and need to take for product of volume and price
-    for ask in asks:
-        max_ask = ask.max()
-        min_ask = asks.min()
-        current_market_price = avg(max_ask, min_ask)
-
-
-
-
-
-get_data('BTCUSD')
 
 
 
